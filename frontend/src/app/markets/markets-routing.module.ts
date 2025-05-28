409282b5a62465e1c76d1c718edc699371c63bd1
Juan Manuel Serrano Rodriguez <jumserranor@udistrital.edu.co>
2025-05-28 16:35:11 -0500

    Update markets-routing.module.ts

--- frontend/src/app/markets/markets-routing.module.ts
+++ frontend/src/app/markets/markets-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MarketsComponent } from './markets/markets.component';
 import { MarketDetailComponent } from './market-detail/market-detail.component';
-// No se requiere guard ya que mencionaste que esta vista es accesible para todos los roles
 
 const routes: Routes = [
   { path: '', component: MarketsComponent },
