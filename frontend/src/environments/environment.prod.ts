25d660c128b549b1a32fc2c5b2348dd5f142ec7b
EngJuanSER <jumserranor@udistrital.edu.co>
2025-06-28 15:23:36 -0500

    managment environment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  apiUrl: "",
+  authApiUrl: "",
+  appName: "",
+  tokenExpiryNotification: 0,
+  newsApiKey: "",
+  alpaca: {
+    baseUrl: "",
+    dataBaseUrl: "",
+    apiKey: "",
+    secretKey: ""
+  },
+  alpacaBroker: {
+    baseUrl: "",
+    apiKey: "",
+    secretKey: "",
+    credentials: "",
+    email: "",
+    password: ""
+  }
+};
