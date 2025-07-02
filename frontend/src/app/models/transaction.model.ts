b570c0f5b37458f3a4034eec87bda94cd3b47db2
santiago-avella <72282837+santiago-avella@users.noreply.github.com>
2025-07-02 00:27:26 -0500

    Delete frontend directory

--- frontend/src/app/models/transaction.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Transaction {
-  id: number;
-  type_transaction: string; 
-  value_transaction: number;
-  date_create: string; // o Date
-  date_update: string; // o Date
-  operation_id?: string;
-  status: string; // o enum
-}
